c09d236d182f886327490b419c057a3c6e3195da
Ali Mughal <alimug38@gmail.com>
2021-01-11 22:59:43 +0500

    Reducer

--- src/Context/transReducer.js
+++ src/Context/transReducer.js
@@ -2,6 +2,12 @@ const TransactionReducer = ((state, action) => {
     switch (action.type) {
         case "ADD TRANSACTION":
             return [action.payload, ...state]
+        case "Delete_Transaction":
+            return {
+                ...state,
+                transactions: state.transactions.filter(transaction => transaction.id !==
+                    action.payload)
+            }
         default:
             return state;
     }
